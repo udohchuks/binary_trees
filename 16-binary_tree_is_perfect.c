197a10d6f9c5829ae93a67b91a5dc1543f0db5f8
udohchuks <urichard132@gmail.com>
2023-08-29 18:18:11 +0100

    task 16 fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,14 +8,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
+
 /**
  * binary_tree_is_perfect - Check if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to check

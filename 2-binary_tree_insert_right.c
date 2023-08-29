6dcf1ef82f4f953a6d59959513a04ceda490e613
udohchuks <urichard132@gmail.com>
2023-08-29 10:26:20 +0100

    fix task3

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,26 +8,22 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node = NULL;
 
 	if (!parent)
 		return (NULL);
+	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
 	node->n = value;
+	node->parent = parent;
+	node->right = NULL;
+	node->left = NULL;
 	if (parent->right)
 	{
 		node->right = parent->right;
-		parent->right = node;
-		node->right->parent = node;
-		node->left = NULL;
-		node->parent = parent;
-		return (node);
+		parent->right->parent = node;
 	}
-	node->parent = parent;
 	parent->right = node;
-	node->right = NULL;
-	node->left = NULL;
-
 	return (node);
 }

5eb4faa54f787030dc267eb423b697c94a6d711e
udohchuks <urichard132@gmail.com>
2023-08-29 18:36:38 +0100

    task16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -33,7 +33,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
 
-	if (l == r)
-		return (1);
-	return (0);
+	return (l == r);
 }

3e11d625ef694a5bd799529aeeebd1a05b51c68f
udohchuks <urichard132@gmail.com>
2023-08-29 13:30:40 +0100

    task 9 and 10

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - height
+ * @tree: tree
+ * Return: size of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (!tree)
+		return (0);
+	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return ((left > right) ? left : right);
+}

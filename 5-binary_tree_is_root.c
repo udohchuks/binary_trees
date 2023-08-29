30b9628d6430c56629cc1968e299a63056ea8e15
udohchuks <urichard132@gmail.com>
2023-08-29 10:52:58 +0100

    task5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - is root
+ * @node: node
+ * Return: 1 or 0
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (node->parent)
+		return (0);
+	return (1);
+}
